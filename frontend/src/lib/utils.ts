022f6b7c697230b2940275e3579bbebadbf8e1c4
Ansh <151115034+Anshweknow@users.noreply.github.com>
2026-07-19 22:45:06 +0530

    Fix Vercel frontend build configuration

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,4 +2,4 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 export const money = (v?: number | null, currency = 'USD') => new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(v ?? 0);
-export const statusLabel = (s: string) => s.replaceAll('_', ' ').toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
+export const statusLabel = (s: string) => s.split('_').join(' ').toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
